fee10513e9e4c4803366bcfc0236922414dc5f21
deanviper <153517604+deanviper@users.noreply.github.com>
2025-07-23 03:16:26 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import WalletConnectProvider from "../components/WalletConnectProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <WalletConnectProvider>
+          {children}
+        </WalletConnectProvider>
       </body>
     </html>
   );
