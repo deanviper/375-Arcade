f860aa2c77cc3facfe0af6d6247e4682c51a5645
deanviper <153517604+deanviper@users.noreply.github.com>
2025-07-22 16:02:09 +0800

    Delete lib/irys.ts

--- lib/irys.ts
+++
@@ -1,8 +0,0 @@
-import { Irys } from '@irys/sdk';
-
-const irys = new Irys({
-  rpcUrl: process.env.NEXT_PUBLIC_IRYS_RPC!,
-  chainId: Number(process.env.NEXT_PUBLIC_IRYS_CHAIN_ID!),
-});
-
-export default irys;
